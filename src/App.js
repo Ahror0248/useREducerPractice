0ce23fe8c774198527caeffc3f6b6db265ed253b
Ahrorbek <akhrorcodrr@gmail.com>
2021-12-02 15:06:38 +0500

    reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
 
 function App() {
+  function reducer(state, action) {
+    switch (action.type) {
+      case "setcount":
+        state = {
+          ...state,
+          count: state.count + 1,
+        };
+        break;
+      case "setcountMinus":
+        state = {
+          ...state,
+          count: state.count - 1,
+        };
+        break;
+      case "setname":
+        state = {
+          ...state,
+          name: "Mahamadaminov",
+        };
+        break;
+    }
+
+    return state;
+  }
+
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    status: "student",
+    name: "Ahrorbek",
+    results: "active",
+  });
+
+  function setCount() {
+    dispatch({ type: "setcount" });
+  }
+
+  function setCountMinus() {
+    dispatch({ type: "setcountMinus" });
+  }
+
+  function setName() {
+    dispatch({ type: "setname" });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={setCount}>+</button>
+      <p>{state.count}</p>
+      <button onClick={setCountMinus}>-</button>
+
+      <p>{state.name}</p>
+      <button onClick={setName}>changeName</button>
     </div>
   );
 }
