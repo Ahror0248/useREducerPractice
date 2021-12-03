32b989815264c7110e1931d228a2f4d537b4c229
Ahrorbek <akhrorcodrr@gmail.com>
2021-12-03 23:15:24 +0500

    reduxPart

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+function reducer(action, state){
+
+  switch(action.type){
+    case 'SET-COUNT':
+      state = {...state, count: action.payload}
+      break
+      case 'SET-NAME': 
+      state = {...state, name: action.payload}
+      default: return state
+  }
+  
+  
+  return state
+}
+
+
+const store = createStore(reducer, {
+  count: 0,
+  name: 'Ahrorbek'
+}, applyMiddleware(logger))
+
+
+store.subscribe(()=>{
+ console.log(store.getState())
+})
+
+store.dispatch({type:'SET-COUNT', payload: 1000})
+// store.dispatch({type:'SET-COUNT', payload: 300})
+// store.dispatch({type:'SET-COUNT', payload: 1200})
+store.dispatch({type:'SET-NAME', payload: 'Abdulloh'})
+store.dispatch({type:'SET-NAME', payload: 'Asadbek'})
+
+// console.log(store.getState());
